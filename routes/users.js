2a783981171fe1470fe212349bcd055e3e5bf328
Notpoze <eirikhusbyn@gmail.com>
2023-04-24 18:53:07 +0200

    Added user DB

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const CyclicDB = require("@cyclic.sh/dynamodb");
+const db = CyclicDB(process.env.CYCLIC_DB);
+let users = db.collection("users");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res, next) => {
+	let item = await users.list();
+	res.send(item);
+});
+
+router.get("/:key", async (req, res, next) => {
+	let item = await users.get(req.params.key);
+	res.send(item);
+});
+
+router.post("/", async (req, res, next) => {
+	const { email, firstName, lastName, age } = req.body;
+	await users.set(email, {
+		firstName: firstName,
+		secondName: lastName,
+		age: age,
+	});
+	res.end();
+});
+
+router.put("/", async (req, res, next) => {
+	const { email, firstName, lastName, age } = req.body;
+	await users.set(email, {
+		firstName: firstName,
+		secondName: lastName,
+		age: age,
+	});
+	res.end();
+});
+
+router.delete("/:key", async (req, res, next) => {
+	await users.delete(req.params.key);
+	res.end();
 });
 
 module.exports = router;
